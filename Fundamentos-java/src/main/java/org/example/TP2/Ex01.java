a2ea38fd8d26d5ca1a7f26114c450da10aa90bdc
Renato Campos Viana <renacv@MacBook-Air-de-Renato.local>
2025-03-06 22:11:39 -0300

    feat: EX01 e EX02 tp 2

---
+++ Fundamentos-java/src/main/java/org/example/TP2/Ex01.java
@@ -0,0 +1,63 @@
+package org.example.TP2;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    private String nomeUsuario;
+    private String nomeMae;
+    private String nomePai;
+    private Integer idade;
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Ex01 ex01 = new Ex01();
+        System.out.println("Digite o seu nome completo: ");
+        ex01.setNomeUsuario(sc.next());
+        System.out.println("Digite o sua idade em formato número (EX: 24)");
+        ex01.setIdade(sc.nextInt());
+        System.out.println("Digite o nome da sua mãe: ");
+        ex01.setNomeMae(sc.next());
+        System.out.println("Digite o nome do seu pai: ");
+        ex01.setNomePai(sc.next());
+        System.out.println("Nome do usuário: " + ex01.getNomeUsuario());
+        System.out.println("Idade do usuário: " + ex01.getIdade());
+        System.out.println("Nome do mae: " + ex01.getNomeMae());
+        System.out.println("Nome do pai: " + ex01.getNomePai());
+        System.out.println(ex01.getNomeUsuario().trim().length() > ex01.getNomeMae().trim().length() ?
+                "Nome do usuário é maior do que o nome da mãe" :
+                "Nome do usuário é menor ou igual do que o nome da mãe");
+
+    }
+}
---
+++ Fundamentos-java/src/main/java/org/example/TP2/Ex02.java
@@ -0,0 +1,26 @@
+package org.example.TP2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex02 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> notas = new ArrayList();
+        for(int i = 0; i<4; i++) {
+            System.out.println("Digite a " + (i+1) + "º nota: ");
+            notas.add(sc.nextDouble());
+        }
+        Double media = (notas.stream().mapToDouble(Double::doubleValue).sum())/4;
+        System.out.println("Sua média foi de: " + media);
+        if(media >= 7){
+            System.out.println("Você foi aprovado!");
+        } else if(media >= 5){
+            System.out.println("Você está de recuperação");
+        } else if (media < 5){
+            System.out.println("Você foi reprovado");
+        }
+    }
+}
